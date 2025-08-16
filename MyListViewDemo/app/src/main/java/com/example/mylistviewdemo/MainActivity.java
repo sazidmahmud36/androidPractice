9666015e1d0b7956dbda76c317a41ded69a703c6
Sazid Mahmud <sazidmahmud36@gmail.com>
2025-08-16 16:43:49 +0600

    ListViewDemo

---
+++ MyListViewDemo/app/src/main/java/com/example/mylistviewdemo/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.mylistviewdemo;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        listView = findViewById(R.id.ListViewId);
+        String[] countryName = getResources().getStringArray(R.array.country);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                MainActivity.this,
+                R.layout.sample_view,
+                R.id.textViewId,
+                countryName);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                String value = countryName[i];
+                Toast.makeText(MainActivity.this, value+" "+i, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
