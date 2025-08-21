9757b2e61e40cfeb1da65dcb759721d2028ed350
Sazid Mahmud <sazidmahmud36@gmail.com>
2025-08-22 02:16:31 +0600

    ProgressBarDemo

---
+++ ProgressBarDemo/app/src/main/java/com/example/progressbardemo/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.progressbardemo;
+
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+    int progress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        progressBar = findViewById(R.id.progressBarId);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                doWork();
+            }
+        });
+        thread.start();
+    }
+    public void doWork(){
+        for (progress=20 ; progress<=100;progress= progress+20){
+            try {
+                Thread.sleep(1000);
+                progressBar.setProgress(progress);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
