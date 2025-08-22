a71dde8b9ca6b65a82acf93a043876672f0d8139
Sazid Mahmud <sazidmahmud36@gmail.com>
2025-08-22 12:10:53 +0600

    progressbar

--- ProgressBarDemo/app/src/main/java/com/example/progressbardemo/MainActivity.java
+++ ProgressBarDemo/app/src/main/java/com/example/progressbardemo/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.progressbardemo;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
@@ -16,6 +18,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // remove the titlebar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //remove the notification Bar
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
