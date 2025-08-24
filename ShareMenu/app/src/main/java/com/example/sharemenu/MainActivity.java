94d5044fd3b5cd8766af771dc2ecca3903a9a4e8
Sazid Mahmud <sazidmahmud36@gmail.com>
2025-08-24 09:51:15 +0600

    ShareMenu

---
+++ ShareMenu/app/src/main/java/com/example/sharemenu/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.sharemenu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater= getMenuInflater();
+        menuInflater.inflate(R.menu.menu_layout,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.shareId){
+//            Toast.makeText(getApplicationContext(), "Share is clicked", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+
+            String sub = "Android Programming App";
+            String body = "This will very helpful to learn android programming.\n com.example.sharemenu;";
+
+            intent.putExtra(Intent.EXTRA_SUBJECT, sub);
+            intent.putExtra(Intent.EXTRA_TEXT, body);
+
+            startActivity(Intent.createChooser(intent,"Share With "));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
